6d1b88d3427b8573a6de2143830daec10ae20771
Victor Teixeira <vyctorbh@gmail.com>
2015-04-17 14:53:12 +0000

    compile file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,10 +27,11 @@ Oauth2ApiSample::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
 
   # Generate digests for assets URLs.
-  config.assets.digest = false
+  config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
@@ -40,7 +41,7 @@ Oauth2ApiSample::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
