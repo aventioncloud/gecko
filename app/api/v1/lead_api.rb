bd0eed4ac66420945e9b4a8887f4f83a4c2c6a8f
Victor Dias Teixeira <victordias@outlook.com>
2017-03-15 00:52:01 +0000

    mobile change

--- app/api/v1/lead_api.rb
+++ app/api/v1/lead_api.rb
@@ -522,19 +522,19 @@ module V1
           leaditem = Lead.joins("LEFT JOIN public.users ON leads.user_id = users.id").joins(:contact).joins(:leadproduct).joins(:leadstatus).where(filter).distinct
           leadcount = leaditem.count
           pages = (leadcount / per_page).ceil
-          lead = Lead.joins("LEFT JOIN public.users ON leads.user_id = users.id").joins(:contact).joins(:leadproduct).joins(:leadstatus).select("users.groups_id, users.name as usuario, leads.*, contacts.id as contact_id, contacts.name, contacts.typecontact as tipo, lead_statuses.id as status_id, lead_statuses.name as status, to_char(leads.updated_at, 'DD/MM/YY HH24:MI') as data, '"+pages.to_s+"' as pages, "+leadcount.to_s+" as qtd_row").where(filter).paginate(:page => params[:page], :per_page => per_page).order(params[:orderby]).distinct
+          lead = Lead.joins("LEFT JOIN public.users ON leads.user_id = users.id").joins(:contact).joins(:leadproduct).joins(:leadstatus).select("users.groups_id, users.name as usuario, leads.*, contacts.id as contact_id, contacts.name as status, contacts.phone, contacts.typecontact as tipo, lead_statuses.id as status_id, lead_statuses.name as status, to_char(leads.updated_at, 'DD/MM/YY HH24:MI') as data, '"+pages.to_s+"' as pages, "+leadcount.to_s+" as qtd_row").where(filter).paginate(:page => params[:page], :per_page => per_page).order(params[:orderby]).distinct
         #Retorna todos os leads do grupo, para responsável
         elsif Group.where(:users_id => @user["id"]).exists?
           ary = Array.new
           leads_group = Group.all_children(ary, @user["groups_id"])
           leadcount = Lead.joins(:user).joins(:contact).joins(:leadstatus).where("users.groups_id IN (?)", leads_group).where(filter).count
           pages = (leadcount / per_page).ceil
-          lead = Lead.joins(:user).joins(:contact).joins(:leadstatus).select("users.groups_id, users.name as usuario, leads.*, contacts.id as contact_id, contacts.name, contacts.typecontact as tipo, lead_statuses.id as status_id, lead_statuses.name as status, to_char(leads.updated_at, 'DD/MM/YY HH24:MI') as data, '"+pages.to_s+"' as pages, "+leadcount.to_s+" as qtd_row").where("users.groups_id IN (?)", leads_group).where(filter).paginate(:page => params[:page], :per_page => per_page).order(params[:orderby])
+          lead = Lead.joins(:user).joins(:contact).joins(:leadstatus).select("users.groups_id, users.name as usuario, leads.*, contacts.id as contact_id, contacts.name, contacts.typecontact as tipo, lead_statuses.id as status_id, lead_statuses.name as status, contacts.phone, to_char(leads.updated_at, 'DD/MM/YY HH24:MI') as data, '"+pages.to_s+"' as pages, "+leadcount.to_s+" as qtd_row").where("users.groups_id IN (?)", leads_group).where(filter).paginate(:page => params[:page], :per_page => per_page).order(params[:orderby])
         #Retorna os Leads para usuário
         else
           leadcount = Lead.joins(:user).joins(:contact).joins(:leadstatus).where("leads.user_id = ?",@user["id"]).count
           pages = (leadcount / per_page).ceil
-          lead = Lead.joins(:user).joins(:contact).joins(:leadstatus).select("users.groups_id, users.name as usuario, leads.*, contacts.id as contact_id, contacts.name, contacts.typecontact as tipo, lead_statuses.id as status_id, lead_statuses.name as status, to_char(leads.updated_at, 'DD/MM/YY HH24:MI') as data, '"+pages.to_s+"' as pages, "+leadcount.to_s+" as qtd_row").where("leads.user_id = ?", @user["id"]).paginate(:page => params[:page], :per_page => per_page).order(params[:orderby])
+          lead = Lead.joins(:user).joins(:contact).joins(:leadstatus).select("users.groups_id, users.name as usuario, leads.*, contacts.id as contact_id, contacts.name, contacts.typecontact as tipo, lead_statuses.id as status_id, lead_statuses.name as status, contacts.phone, to_char(leads.updated_at, 'DD/MM/YY HH24:MI') as data, '"+pages.to_s+"' as pages, "+leadcount.to_s+" as qtd_row").where("leads.user_id = ?", @user["id"]).paginate(:page => params[:page], :per_page => per_page).order(params[:orderby])
         end
         lead
         #@lead
@@ -598,19 +598,19 @@ module V1
           leaditem = Lead.joins("LEFT JOIN public.users ON leads.user_id = users.id").joins(:contact).joins(:leadproduct).joins(:leadstatus).where(filter).distinct
           leadcount = leaditem.count
           pages = (leadcount / per_page).ceil
-          lead = Lead.joins("LEFT JOIN public.users ON leads.user_id = users.id").joins(:contact).joins(:leadproduct).joins(:leadstatus).select("users.groups_id, users.name as usuario, leads.*, contacts.id as contact_id, contacts.name, contacts.typecontact as tipo, lead_statuses.id as status_id, lead_statuses.name as status, to_char(leads.updated_at + INTERVAL '1 HOURS', 'DD/MM/YY HH24:MI') as data, '"+pages.to_s+"' as pages, "+leadcount.to_s+" as qtd_row, '"+filename+"' as file_key, contacts.*").where(filter).paginate(:page => params[:page], :per_page => per_page).order(params[:orderby]).distinct
+          lead = Lead.joins("LEFT JOIN public.users ON leads.user_id = users.id").joins(:contact).joins(:leadproduct).joins(:leadstatus).select("users.groups_id, users.name as usuario, leads.*, contacts.id as contact_id, contacts.name, contacts.typecontact as tipo, contacts.phone as phone, lead_statuses.id as status_id, lead_statuses.name as status, to_char(leads.updated_at + INTERVAL '1 HOURS', 'DD/MM/YY HH24:MI') as data, '"+pages.to_s+"' as pages, "+leadcount.to_s+" as qtd_row, '"+filename+"' as file_key, contacts.*").where(filter).paginate(:page => params[:page], :per_page => per_page).order(params[:orderby]).distinct
           #Retorna todos os leads do grupo, para responsável
         elsif Group.where(:users_id => @user["id"]).exists?
           ary = Array.new
           leads_group = Group.all_children(ary, @user["groups_id"])
           leadcount = Lead.joins(:user).joins(:contact).joins(:leadstatus).where("users.groups_id IN (?)", leads_group).where(filter).count
           pages = (leadcount / per_page).ceil
-          lead = Lead.joins(:user).joins(:contact).joins(:leadstatus).select("users.groups_id, users.name as usuario, leads.*, contacts.id as contact_id, contacts.name, contacts.typecontact as tipo, lead_statuses.id as status_id, lead_statuses.name as status, to_char(leads.updated_at + INTERVAL '1 HOURS', 'DD/MM/YY HH24:MI') as data, '"+pages.to_s+"' as pages, "+leadcount.to_s+" as qtd_row, '"+filename+"' as file_key, contacts.*").where("users.groups_id IN (?)", leads_group).where(filter).paginate(:page => params[:page], :per_page => per_page).order(params[:orderby])
+          lead = Lead.joins(:user).joins(:contact).joins(:leadstatus).select("users.groups_id, users.name as usuario, leads.*, contacts.id as contact_id, contacts.name, contacts.typecontact as tipo, contacts.phone as phone, lead_statuses.id as status_id, lead_statuses.name as status, to_char(leads.updated_at + INTERVAL '1 HOURS', 'DD/MM/YY HH24:MI') as data, '"+pages.to_s+"' as pages, "+leadcount.to_s+" as qtd_row, '"+filename+"' as file_key, contacts.*").where("users.groups_id IN (?)", leads_group).where(filter).paginate(:page => params[:page], :per_page => per_page).order(params[:orderby])
           #Retorna os Leads para usuário
         else
           leadcount = Lead.joins(:user).joins(:contact).joins(:leadstatus).where("leads.user_id = ?",@user["id"]).count
           pages = (leadcount / per_page).ceil
-          lead = Lead.joins(:user).joins(:contact).joins(:leadstatus).select("users.groups_id, users.name as usuario, leads.*, contacts.id as contact_id, contacts.name, contacts.typecontact as tipo, lead_statuses.id as status_id, lead_statuses.name as status, to_char(leads.updated_at + INTERVAL '1 HOURS', 'DD/MM/YY HH24:MI') as data, '"+pages.to_s+"' as pages, "+leadcount.to_s+" as qtd_row, '"+filename+"' as file_key, contacts.*").where("leads.user_id = ?", @user["id"]).paginate(:page => params[:page], :per_page => per_page).order(params[:orderby])
+          lead = Lead.joins(:user).joins(:contact).joins(:leadstatus).select("users.groups_id, users.name as usuario, leads.*, contacts.id as contact_id, contacts.name, contacts.typecontact as tipo, contacts.phone as phone, lead_statuses.id as status_id, lead_statuses.name as status, to_char(leads.updated_at + INTERVAL '1 HOURS', 'DD/MM/YY HH24:MI') as data, '"+pages.to_s+"' as pages, "+leadcount.to_s+" as qtd_row, '"+filename+"' as file_key, contacts.*").where("leads.user_id = ?", @user["id"]).paginate(:page => params[:page], :per_page => per_page).order(params[:orderby])
         end
 
         mab = Markaby::Builder.new
--- app/api/v1/login_api.rb
+++ app/api/v1/login_api.rb
@@ -24,8 +24,17 @@ module V1
         password = BCrypt::Engine.hash_secret("#{params[:password]}#{resource.class.pepper}", bcrypt.salt)
         valid = Devise.secure_compare(password, resource.encrypted_password)
         # resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#new")
+        ambiente = 'rio'
         if valid
-          app = Doorkeeper::Application.where(uid: '03dbaf9c6f099e449a3d8ad2b4903727be168a56c2e890145037154ce66225bd').first
+          if resource.accounts_id == 13
+            app = Doorkeeper::Application.where(uid: '03dbaf9c6f099e449a3d8ad2b4903727be168a56c2e890145037154ce66225bd').first
+          elsif resource.accounts_id == 14
+            ambiente = 'manaus'
+            app = Doorkeeper::Application.where(uid: '5290cf3ec63badd8e7b2bb2032cf170ceb3a65a5fd4be1f81fd8b89c11e8ff03').first
+          else 
+            ambiente = 'dev'
+            app = Doorkeeper::Application.where(uid: '6551a588a3e6842386539720e80c13e2cfc63bb69ab897f1f37aa88de20c1085').first       
+        end
           access_token = Doorkeeper::AccessToken.create!({
             :application_id     => app.id,
             :resource_owner_id  => resource.id,
@@ -34,7 +43,7 @@ module V1
             :use_refresh_token  => Doorkeeper.configuration.refresh_token_enabled?
           })
           @token_response = Doorkeeper::OAuth::TokenResponse.new(access_token)
-          {:code => 201, :token => @token_response.body, :user => resource}
+          {:code => 201, :token => @token_response.body, :user => resource, :ambiente => ambiente}
         else
            {:code => 401, :error => 'Invalid Email Address or Password. Password is case sensitive.'}   
         end
